e6de81c1c687b08746b3813190c08bad4c9513aa
mojsx <mojsx.dev@gmail.com>
2024-11-06 23:16:28 +0100

    refactor[ui]: extract message ui and logic to its own component.

--- src/ui/molecules/ChatMessages.tsx
+++ src/ui/molecules/ChatMessages.tsx
@@ -1,17 +1,6 @@
 import { MessageProps } from '../../types';
-import { Loader, WelcomeMessage } from '../atoms';
+import { Message, WelcomeMessage } from '../atoms';
 import { useEffect, useRef } from 'react';
-
-import geppettoIcon from '../../assets/bot.webp';
-import userIcon from '../../assets/user.webp';
-
-import ReactMarkdown from 'react-markdown';
-import { nanoid } from 'nanoid';
-import rehypeHighlight from 'rehype-highlight';
-import rehypeKatex from 'rehype-katex';
-import rehypeRaw from 'rehype-raw';
-import remarkBreaks from 'remark-breaks';
-import remarkMath from 'remark-math';
 
 interface ChatMessagesProps {
   messages: MessageProps[];
@@ -42,70 +31,19 @@ function ChatMessages({ messages, isFetching }: ChatMessagesProps) {
           </h1>
         )}
 
-        {messages.map(({ role, content }, index) => (
-          <div
-            key={index}
-            className={`${
-              role == 'user'
-                ? 'bg-white md:ml-[150px] lg:ml-[200px] xl:ml-40 border-2 border-slate-100'
-                : 'bg-blue-50 border-2 border-blue-100'
-            } px-2 py-8 mb-4 md:w-[80%] xl:w-[80%] rounded-xl`}
-            data-testid="chat-message"
-          >
-            <header
-              className={`flex flex-row ${
-                role == 'geppetto' ? 'justify-start' : 'justify-end'
-              }`}
-              data-testid="chat-message-header"
-            >
-              <img
-                src={role == 'geppetto' ? geppettoIcon : userIcon}
-                width={36}
-                alt={`${role} icon`}
-                data-testid="chat-message-avatar"
-              />
-              <h3 className="mt-2 ml-2">
-                {role == 'geppetto' ? 'Geppetto' : ''}
-              </h3>
-            </header>
+        {messages.map(({ role, content }, index) => {
+          const isLastMessage = messages.length == index + 1;
 
-            {role == 'geppetto' &&
-            isFetching &&
-            messages.length == index + 1 ? (
-              <Loader />
-            ) : role == 'geppetto' ? (
-              <ReactMarkdown
-                key={nanoid()}
-                remarkPlugins={[remarkBreaks, remarkMath]}
-                rehypePlugins={[rehypeRaw, rehypeHighlight, rehypeKatex]}
-                components={{
-                  a: (a, ...props) => (
-                    <a
-                      href={a.href}
-                      className="text-white bg-blue-400 underline visited:bg-purple-400"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      {...props}
-                    >
-                      {a.children}
-                    </a>
-                  ),
-                }}
-                className="text-wrap"
-                data-testid="geppetto-message-content"
-              >
-                {content}
-              </ReactMarkdown>
-            ) : (
-              <div
-                className="text-wrap whitespace-pre-line"
-                data-testid="user-message-content"
-              >
-                {content}
-              </div>
-            )}
-          </div>
-        ))}
+          return (
+            <Message
+              key={index}
+              isFetching={isFetching}
+              isLastMessage={isLastMessage}
+              role={role}
+              content={content}
+            />
+          );
+        })}
       </div>
     </div>
   );
