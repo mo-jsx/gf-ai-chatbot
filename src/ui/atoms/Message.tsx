49fb9040db2b17c60f6d1f9b41ff458a65b67825
mojsx <mojsx.dev@gmail.com>
2024-11-07 00:15:47 +0100

    styles: update message border colors, message header paddings.

--- src/ui/atoms/Message.tsx
+++ src/ui/atoms/Message.tsx
@@ -1,6 +1,5 @@
 import { nanoid } from 'nanoid';
 import ReactMarkdown from 'react-markdown';
-import rehypeHighlight from 'rehype-highlight';
 import rehypeKatex from 'rehype-katex';
 import rehypeRaw from 'rehype-raw';
 import remarkBreaks from 'remark-breaks';
@@ -23,13 +22,13 @@ export default function Message(props: IMessage) {
     <div
       className={`${
         role == 'user'
-          ? 'bg-white md:ml-[150px] lg:ml-[200px] xl:ml-40 border-2 border-slate-100'
+          ? 'bg-white md:ml-[150px] lg:ml-[200px] xl:ml-40 border-2 border-slate-200'
           : 'bg-blue-50 border-2 border-blue-100'
-      } px-2 py-8 mb-4 md:w-[80%] xl:w-[80%] rounded-xl`}
+      } px-2 py-4 mb-4 md:w-[80%] xl:w-[80%] rounded-xl`}
       data-testid="chat-message"
     >
       <header
-        className={`flex flex-row ${
+        className={`flex flex-row mb-2 ${
           role == 'geppetto' ? 'justify-start' : 'justify-end'
         }`}
         data-testid="chat-message-header"
@@ -49,7 +48,7 @@ export default function Message(props: IMessage) {
         <ReactMarkdown
           key={nanoid()}
           remarkPlugins={[remarkBreaks, remarkMath]}
-          rehypePlugins={[rehypeRaw, rehypeHighlight, rehypeKatex]}
+          rehypePlugins={[rehypeRaw, rehypeKatex]}
           components={{
             a: (a, ...props) => (
               <a
