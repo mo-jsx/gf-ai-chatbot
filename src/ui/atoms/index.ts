04dfbded18ba08866c2c326a95134430c406194b
mojsx <mojsx.dev@gmail.com>
2024-11-06 23:15:17 +0100

    chores: add single export for Message component.

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as SendButton } from './SendButton/SendButton';
 export { default as Loader } from './Loader';
 export { default as PromptInput } from './PromptInput';
 export { default as WelcomeMessage } from './WelcomeMessage';
+export { default as Message } from './Message';
