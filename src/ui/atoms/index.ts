19ba339447845dac32615c634f8a5cb450ddf4c1
mojsx <mojsx.dev@gmail.com>
2024-11-06 22:34:45 +0100

    chores: add single exports for SendButton, Loader, PromptInput, WelcomeMessage components.

---
+++ src/ui/atoms/index.ts
@@ -0,0 +1,4 @@
+export { default as SendButton } from './SendButton/SendButton';
+export { default as Loader } from './Loader';
+export { default as PromptInput } from './PromptInput';
+export { default as WelcomeMessage } from './WelcomeMessage';
