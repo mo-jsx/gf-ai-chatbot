84331ff9bfd020fba68e8c9e7b27d9fcef91a636
mojsx <mojsx.dev@gmail.com>
2024-11-06 22:30:44 +0100

    component: add PromptInput component.

---
+++ src/ui/atoms/PromptInput.tsx
@@ -0,0 +1,26 @@
+interface PromptInputProps {
+  textareaRef: any;
+  newPrompt: string;
+  setNewPrompt: React.Dispatch<React.SetStateAction<string>>;
+  handleKeyDown: any;
+}
+
+function PromptInput(props: PromptInputProps) {
+  const { textareaRef, newPrompt, setNewPrompt, handleKeyDown } = props;
+
+  return (
+    <textarea
+      ref={textareaRef}
+      className="h-12 px-4 py-2 resize-none w-[100%] border-2 border-blue-400 rounded-full"
+      rows={1}
+      value={newPrompt}
+      onChange={(e) => setNewPrompt(e.target.value)}
+      onKeyDown={handleKeyDown}
+      name="chat_input"
+      id="chat_input"
+      placeholder="Start typing to interact with Geppetto.."
+    />
+  );
+}
+
+export default PromptInput;
