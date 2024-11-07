b428b0f6c2afa8ad8750d6b92a134c402176d166
mojsx <mojsx.dev@gmail.com>
2024-11-07 06:45:26 +0100

    chores: add single export for ResetChat component.

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -3,3 +3,4 @@ export { default as Loader } from './Loader';
 export { default as PromptInput } from './PromptInput';
 export { default as WelcomeMessage } from './WelcomeMessage';
 export { default as Message } from './Message';
+export { default as ResetChat } from './ResetChat';
