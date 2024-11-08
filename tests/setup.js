fe575fd25fa9a54fae43b020963f8b683b0e2ea0
mojsx <mojsx.dev@gmail.com>
2024-11-08 07:35:01 +0100

    test: setup vitest.

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,6 @@
-import { afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/vitest";
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
 
 // cleans the dom after each test case
 afterEach(() => {
